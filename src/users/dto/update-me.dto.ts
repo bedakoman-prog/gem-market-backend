007036a5a18428144df23b0ef223e100d3432701
bedakoman-prog <bedakoman@gmail.com>
2026-09-15 19:30:43 +0000

    Create update-me.dto.ts

---
+++ src/users/dto/update-me.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class UpdateMeDto {
+    @IsOptional()
+    @IsString()
+    @MaxLength(120)
+    name?: string;
+
+  @IsOptional()
+    @IsEmail()
+    email?: string;
+
+  @IsOptional()
+    @IsString()
+    @MaxLength(120)
+    city?: string;
+
+  @IsOptional()
+    isSeller?: boolean;
+}

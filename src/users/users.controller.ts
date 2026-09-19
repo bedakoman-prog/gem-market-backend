f3b77523372b5a770ac53528d24e08d079b3d75a
bedakoman-prog <bedakoman@gmail.com>
2026-09-19 18:54:35 +0000

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,25 @@
-import { Body, Controller, Get, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 import { AuthenticatedUser } from '../auth/strategies/jwt.strategy';
 import { UsersService } from './users.service';
 import { UpdateMeDto } from './dto/update-me.dto';
 
 @Controller('users')
-  export class UsersController {
-    constructor(private usersService: UsersService) {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
 
   @Get('me')
-    me(@CurrentUser() user: AuthenticatedUser) {
-          return this.usersService.me(user.id);
-    }
+  me(@CurrentUser() user: AuthenticatedUser) {
+    return this.usersService.me(user.id);
+  }
 
   @Patch('me')
-    updateMe(@CurrentUser() user: AuthenticatedUser, @Body() dto: UpdateMeDto) {
-          return this.usersService.updateMe(user.id, dto);
-    }
+  updateMe(@CurrentUser() user: AuthenticatedUser, @Body() dto: UpdateMeDto) {
+    return this.usersService.updateMe(user.id, dto);
+  }
+
+  @Post('bootstrap-admin')
+  bootstrapAdmin(@CurrentUser() user: AuthenticatedUser) {
+    return this.usersService.bootstrapAdmin(user.id);
+  }
 }

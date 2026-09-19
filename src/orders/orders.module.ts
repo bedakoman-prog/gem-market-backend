51800be1d05ed726dfd309aea32246fe89522726
bedakoman-prog <bedakoman@gmail.com>
2026-09-19 10:19:15 +0000

    Feat : enregistrer OrdersEscrowScheduler dans OrdersModule

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PaymentsModule } from '../payments/payments.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { OrdersEscrowScheduler } from './orders-escrow.scheduler';
 
 @Module({
   imports: [PaymentsModule],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersEscrowScheduler],
 })
 export class OrdersModule {}

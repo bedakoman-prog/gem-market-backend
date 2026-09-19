fcd93f4e284a91eb8130ecd54fedffb4a86c4c71
bedakoman-prog <bedakoman@gmail.com>
2026-09-19 10:53:58 +0000

    Feat : brancher PromoPeriodModule dans ListingsModule

--- src/listings/listings.module.ts
+++ src/listings/listings.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PromoPeriodModule } from '../common/promo/promo-period.module';
 import { ListingsController } from './listings.controller';
 import { ListingsService } from './listings.service';
 
 @Module({
+  imports: [PromoPeriodModule],
   controllers: [ListingsController],
   providers: [ListingsService],
   exports: [ListingsService],

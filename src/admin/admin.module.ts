6fe7a3544a009d41b4d5e23efcf5b859ce4ed70e
bedakoman-prog <bedakoman@gmail.com>
2026-09-20 13:44:56 +0000

    Feat : imports PaymentsModule/OrdersModule dans AdminModule

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { AdminGuard } from '../common/guards/admin.guard';
+import { PaymentsModule } from '../payments/payments.module';
+import { OrdersModule } from '../orders/orders.module';
 
 @Module({
+  imports: [PaymentsModule, OrdersModule],
   controllers: [AdminController],
   providers: [AdminService, AdminGuard],
 })
-export class AdminModule {}
+  export class AdminModule {}
